b6b4817e6f48d934e2a9ce09c00298ae926aa79e
Kshitij <chaudharykshitij49@gmail.com>
2024-10-12 19:51:35 -0400

    Added comments

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#importing required libraries
 from flask import Flask, render_template
 from pymongo import MongoClient
 from dotenv import load_dotenv
@@ -13,15 +14,18 @@ client = MongoClient(os.getenv('MONGO_URI'))
 db = client.shop_db 
 products_collection = db.products
 
+# Route for the home page
 @app.route('/')
 def home():
     return render_template('home.html')
 
+# Route for the products page
 @app.route('/products')
 def get_products():
     products = list(products_collection.find())
     return render_template('products.html', products=products)
 
-
+# Run the application 
 if __name__ == '__main__':
+#Running the app in debug mode for development
     app.run(debug = True)
