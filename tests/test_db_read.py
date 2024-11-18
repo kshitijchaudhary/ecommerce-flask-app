21d54fccaa37c113f5c2fcb7b2a49065a52d6be8
Kshitij <chaudharykshitij49@gmail.com>
2024-11-18 12:27:44 -0500

    fix and config the mongo_uri in db read and write test files

--- tests/test_db_read.py
+++ tests/test_db_read.py
@@ -9,12 +9,20 @@ load_dotenv()
 class TestDatabase(unittest.TestCase):
     
     def test_mongo_read_connection(self):
-        MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
-        MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+        # MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
+        # MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
         
-        # Connect to MongoDB
-        client = MongoClient(f'mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@cluster0.deqr1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        # # Connect to MongoDB
+        # client = MongoClient(f'mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@cluster0.deqr1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         
+        mongo_uri = os.getenv('MONGO_URI')
+
+        # Ensure MongoDB username and password are retrieved
+        if not mongo_uri:
+            raise ValueError("MongoDB credentials are not set in the .env file")
+        
+        client = MongoClient(mongo_uri)
+
         # Test MongoDB connection
         try:
             client.admin.command('ping')
